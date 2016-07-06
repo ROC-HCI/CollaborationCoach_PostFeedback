f6b3d922014323c9d274058444d752fc945a2a33
jwhite37 <jwhite37@ur.rochester.edu>
2016-07-06 18:52:20 -0400

    Revert Changes

--- Real-Faces/static/js/socketMain.js
+++ Real-Faces/static/js/socketMain.js
@@ -5,81 +5,12 @@ var RealSocket = function (app) {
   this.lastRecordedPlayerTranslations = {};
   
   var context = this;
-  
-  var player_1_translation = {
-    position: {x:40, y:0, z:0},
-    rotation: {x:0, y:0}
-  };
-  
-  var player_2_translation = {
-    position: {x:-40, y:0, z:0},
-    rotation: {x:0, y:0}
-  };
-  
-  var player_3_translation = {
-    position: {x:0, y:40, z:0},
-    rotation: {x:0, y:0}
-  };
-  
-  var player_4_translation = {
-    position: {x:0, y:-40, z:0},
-    rotation: {x:0, y:0}
-  };
-  
-  var player_5_translation = {
-    position: {x:20, y:20, z:0},
-    rotation: {x:0, y:0}
-  };
-  
-  var player_6_translation = {
-    position: {x:-20, y:-20, z:0},
-    rotation: {x:0, y:0}
-  };  
-  
-  var default_translation =  {
-    position: {x:0, y:0, z:0},
-    rotation: {x:0, y:0}
-  };
-  
+ 
   //YOUR PLAYER UPDATES TO SERVER
-  /*
   this.yourPlayerTranslation = {
     position: {x:0, y:10, z:0},
     rotation: {x:0, y:0}
-  };
-  */
-  
-  var spawn_selection = Math.floor((Math.random() * 6) + 1);
-  
-  switch(spawn_selection) 
-  {
-    case 1:
-        this.yourPlayerTranslation = player_1_translation;
-		console.log("SPAWN SELECTION #1");
-        break;
-    case 2:
-        this.yourPlayerTranslation = player_2_translation;
-		console.log("SPAWN SELECTION #2");
-        break;
-	case 3:
-        this.yourPlayerTranslation = player_3_translation;
-		console.log("SPAWN SELECTION #3");
-        break;
-	case 4:
-        this.yourPlayerTranslation = player_4_translation;
-		console.log("SPAWN SELECTION #4");
-        break;
-	case 5:
-        this.yourPlayerTranslation = player_5_translation;
-		console.log("SPAWN SELECTION #5");
-        break;
-	case 6:
-        this.yourPlayerTranslation = player_6_translation;
-		console.log("SPAWN SELECTION #6");
-        break;
-    default:
-        this.yourPlayerTranslation = default_translation;
-}
+  }; 
 
   this.translated = false;
 
@@ -107,7 +38,7 @@ var RealSocket = function (app) {
 
   this.socketio.on('new_client', function(clientID){
     console.log('new player socket', clientID)
-    context.lastRecordedPlayerTranslations[clientID] = {position:{x:50, y:50, z:0}, rotation:{x:0,y:0}};
+    context.lastRecordedPlayerTranslations[clientID] = {position:{x:0, y:10, z:10}, rotation:{x:0,y:0}};
     //otherPlayerUpdates will hear this and create a new player
     playerEvents.emit('new_player', [clientID]);
   });
