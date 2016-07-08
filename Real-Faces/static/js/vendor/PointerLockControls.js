9810fee3b009077643eeaced7f0b2bfe4277d853
jwhite37 <jwhite37@ur.rochester.edu>
2016-07-08 12:39:26 -0400

    Camera Rotation

--- Real-Faces/static/js/vendor/PointerLockControls.js
+++ Real-Faces/static/js/vendor/PointerLockControls.js
@@ -160,7 +160,7 @@ THREE.PointerLockControls = function ( camera, sceneVars, positiveBoundaryX, neg
   this.setPosition = function(x_pos, z_pos){
 	  yawObject.position.setX(x_pos);
 	  yawObject.position.setZ(z_pos);
-	  //yawObject.lookAt(0,yawObject.position.y,0);
+	  yawObject.rotation.set(0,0,0);
   }
 
   this.getObject = function () {
