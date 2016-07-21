fd97a2ec32ea8d6ea5bb13bf3268df64c08e575e
jwhite37 <jwhite37@ur.rochester.edu>
2016-07-21 17:53:16 -0400

    script test

--- runscript.sh
+++ runscript.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
 echo "The script starts now"
+
 echo "Start - Affdex"
 $HOME/build/video-demo/./video-demo -d $HOME/affdex-sdk/data -l sdk_lnova@u.rochester.edu.license -i Data/Ru.webm
 echo "Finish - Affdex"
+
+echo "Start - .wav Conversion"
+for file in Data/*.webm;
+	do ffmpeg -i "$file".webm -qscale 0 "$file".wav;
+echo "Finish - .wav Conversion"
 
 echo "Start - Praat"
 
