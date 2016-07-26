fc18c1196c702e870ddf77557242ad94ab9d7645
jwhite37 <jwhite37@ur.rochester.edu>
2016-07-26 18:09:24 -0400

    MongoTesting

---
+++ server_api.php
@@ -0,0 +1,26 @@
+<?php
+
+$connection = new MongoClient();
+
+//===========================================================
+// Test code to insert a collection into a test database
+// and dump it.
+//===========================================================
+$database = $connection->selectDB("RConfTest");
+
+$collection = $database['test_collection'];
+
+$test_document = array("First Index" => "First Value",
+                       "Second Index" => "Second Value");
+					   
+$collection->insert($test_document);
+
+$collection2 = $database['test_collection'];
+
+echo var_dump($collection2);
+
+//=============================================================
+
+
+
+?>
