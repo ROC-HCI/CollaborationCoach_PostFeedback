efd6767987904ce2474cdaab6c96b1ba475764b2
jwhite37 <jwhite37@ur.rochester.edu>
2016-07-27 17:56:38 -0400

    bash script test

--- runscript.sh
+++ runscript.sh
@@ -2,7 +2,7 @@
 
 session_id=$1
 
-echo "Session Script Starting for " + session_id
+echo "Session Script Starting for "+$session_id
 
 echo "Start - Affdex"
 #$HOME/build/video-demo/./video-demo -d $HOME/affdex-sdk/data -l sdk_lnova@u.rochester.edu.license -i Data/Ru.webm
@@ -13,10 +13,10 @@ echo "Start - Praat"
 
 py_args=""
 for i in Data/session_id_*.wav.TextGrid;
-	do py_args+=" "+i;
+	do $py_args+=" "+i;
 done
 
-echo py_args;
+echo $py_args;
 #python fileparser.py Data/Ru.wav.TextGrid Data/Luis.wav.TextGrid Data/Yichen.wav.TextGrid
 echo "Finish - Praat"
 
