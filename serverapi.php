e81dda6a2bbcf6a0c3323f3d8c9db4ee0344d444
jwhite37 <jwhite37@ur.rochester.edu>
2016-07-29 18:18:24 -0400

    server api test for query

--- serverapi.php
+++ serverapi.php
@@ -1,14 +1,14 @@
 <?php
-error_reporting(E_ALL);
-
 session_start();
 
 $connection = new MongoClient();
+$database = $connection->selectDB("rocconf");
 
 //===========================================================
 // Test code to insert a collection into a test database
 // and dump it.
 //===========================================================
+/*
 $database = $connection->selectDB("RConfTest");
 
 $collection = $database->selectCollection('test_collection');
@@ -22,9 +22,24 @@ $collection2 = $database->selectCollection('test_collection');
 
 echo var_dump($collection2) . "<br/><br/>";
 echo $collection2->count() . " documents";
-
+*/
 //=============================================================
 
+
+if($_GET['mode'] == 'participation')
+{
+	$session_key = $_GET['session_key'];
+	
+	$collection = $database=>selectCollection('participation');
+	$query = array('session_key' => $session_key);
+	
+	$cursor = $collection->find($query);
+	
+	foreach($cursor as $document)
+	{
+		echo json_encode($document);
+	}	
+}
 
 
 ?>
