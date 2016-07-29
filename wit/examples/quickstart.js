f35fd7158535ce3739e8bac5f043229576378d23
yichen_0@hotmail.com <ylu021>
2016-07-28 22:29:42 -0400

    wit.ai working, the bug was it needed unique id for testing purpose fxx

--- wit/examples/quickstart.js
+++ wit/examples/quickstart.js
@@ -1,23 +1,18 @@
 'use strict';
 
 let Wit = null;
+let log = null;
 try {
   // if running from repo
   Wit = require('../').Wit;
+  log = require('../').log;
 } catch (e) {
   Wit = require('node-wit').Wit;
+  log = require('node-wit').log;
 }
 
-// const accessToken = (() => {
-//   if (process.argv.length !== 3) {
-//     console.log('usage: node examples/quickstart.js <wit-access-token>');
-//     process.exit(1);
-//   }
-//   return process.argv[2];
-// })();
-
-// Quickstart example
-// See https://wit.ai/ar7hur/quickstart
+const express = require('express');
+const app = express();
 
 const firstEntityValue = (entities, entity) => {
   const val = entities && entities[entity] &&
@@ -33,26 +28,43 @@ const firstEntityValue = (entities, entity) => {
 
 const actions = {
   send(request, response) {
-    console.log('sending...', JSON.stringify(response));
-    return Promise.resolve();
+    const {sessionId, context, entities} = request;
+    const {text, quickreplies} = response;
+    return new Promise(function(resolve, reject) {
+      console.log('sending...', JSON.stringify(response));
+      return resolve();
+    });
   },
   getForecast({context, entities}) {
-    console.log('im here');
     return new Promise(function(resolve, reject) {
       var location = firstEntityValue(entities, 'location')
-      context.forecast = 'sunny in ' + location; // we should call a weather API here
+      if (location) {
+        context.forecast = 'sunny in ' + location; // we should call a weather API here
+        delete context.missingLocation;
+      } else {
+        context.missingLocation = true;
+        delete context.forecast;
+      }
       return resolve(context);
     });
   },
 };
 
-const client = new Wit({accessToken: '7QME7JWGVXF2D6R6TKR54ACHMMVARTKL', actions});
-const sessionId = 'testingland';
-client.runActions(sessionId, 'How is the weather in London?', {})
+const logger = new log.Logger(log.DEBUG);
+
+const sessionId = require('node-uuid').v1();
+const context0 = {};
+const client = new Wit({accessToken: 'A6IWPIVFV5EPHNFSMKVY6QDVBE3KBT55', actions, logger})
+client.runActions(sessionId, 'Whats the weather in Paris', context0)
 .then((context1) => {
-  console.log('The session state is now: ' + JSON.stringify(context1));
-  //**no reply for some reason, check wit.js search for runActions and play with continueActions
+  console.log('The session state is now: ' + JSON.stringify(context1, null, 2));
+  return client.runActions(sessionId, 'and in London?', context1);
+//   // return client.runActions(sessionId, 'The weather is', context1)
+})
+.then((context2) => {
+  console.log('The session state is now: ' + JSON.stringify(context2, null, 2));
 })
 .catch((e) => {
   console.log('Oops! Got an error: ' + e);
 });
+
