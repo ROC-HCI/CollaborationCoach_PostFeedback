be3fcb7fb4e8a652fe88a2b5a14489b1b2838ff2
jwhite37 <jwhite37@ur.rochester.edu>
2016-07-31 16:42:54 -0400

    ToneAnalyzer separated from SpeechRecognition
    Some DB stuff has been added as well

---
+++ ToneAnalyzer.py
@@ -0,0 +1,83 @@
+#==================================================================================
+# IBM Bluemix Tone Analyzer for the RocConf Project
+#    - Jeffery A. White - July 2016
+#
+# Usage - ToneAnalyzer.py {SESSION_KEY} {USER}
+#==================================================================================
+import pymongo
+from pymongo import MongoClient
+import pprint
+import sys
+import os, base64
+import json
+
+from urllib import urlencode
+from urllib2 import Request, urlopen, URLError, HTTPError
+
+class RequestError(Exception): pass
+
+client = MongoClient()
+database = client['rocconf']
+collection = database['transcript']
+
+pp = pprint.PrettyPrinter(indent=2)
+
+#==================================================================================
+# Access keys for using IBM Bluemix. Generate these through a Bluemix Account
+# http://www.ibm.com/cloud-computing/bluemix/
+#==================================================================================
+IBM_TONE_USERNAME = "459ca71f-efec-4a3c-8313-e07816be33d9"
+IBM_TONE_PASSWORD = "yVjLxiSri7As"
+
+#==================================================================================
+# Get the text transcript for this user from MongoDB
+#==================================================================================
+def get_transcript(session_id, user_id):
+    transcript_text = "I'm creating a test bit of text before worry about pulling from the MongoDB. This is because we haven't nailed down the speech recognition bit just yet."
+    
+    return transcript_text
+    
+#==================================================================================
+# Run this on a text based transcript to obtain tone analysis, add this
+# analysis data to the tone collection
+#==================================================================================
+def process_tone(transcript_text, session_id, user_id):
+    url = "https://gateway.watsonplatform.net/tone-analyzer/api/v3/tone?{0}" . format(urlencode({
+            "version": "2016-05-19",
+            "text": transcript_text
+        }))
+        
+    request = Request(url, headers = {"Content-Type": "application/json"})
+        
+    if hasattr("", "encode"):
+        authorization_value = base64.standard_b64encode("{0}:{1}".format(IBM_TONE_USERNAME, IBM_TONE_PASSWORD).encode("utf-8")).decode("utf-8")
+    else:
+        authorization_value = base64.standard_b64encode("{0}:{1}".format(IBM_TONE_USERNAME, IBM_TONE_PASSWORD))
+    
+    request.add_header("Authorization", "Basic {0}" . format(authorization_value))
+    
+    try:
+        response = urlopen(request)
+    except HTTPError as e:
+        raise RequestError("tone analysis request failed: {0}".format(getattr(e, "reason", "status {0}".format(e.code)))) # use getattr to be compatible with Python 2.6
+    except URLError as e:
+        raise RequestError("tone analysis connection failed: {0}".format(e.reason))
+        
+    response_text = response.read().decode("utf-8")
+    result = json.loads(response_text)
+    
+    collection = database['tone']
+    
+    final_dict = {}
+    final_dict["session_key"] = session_id
+    final_dict["user"] = user_id
+    final_dict["data"] = result
+    
+    pp.pprint(collection.insert_one(final_dict).inserted_id)
+
+#=======================================================
+# Main Caller
+#=======================================================
+if __name__ == "__main__":
+    transcript = get_transcript(sys.argv[1], sys.argv[2])
+    process_tone(transcript, sys.argv[1], sys.argv[2])
