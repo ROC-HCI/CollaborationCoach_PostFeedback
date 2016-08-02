396e4a5dcbf7225d2a10b1ccc07c4a91245b21fc
rzhao2 <rzhao2@u.rochester.edu>
2016-08-02 15:27:34 -0400

    try js xmlhttprequest

--- feedback.php
+++ feedback.php
@@ -1,15 +1,22 @@
 <?php
-	$url = 'https://conference.eastus.cloudapp.azure.com/RocConf/serverapi.php?mode=affdexmerge&session_key=test-key-test&user=Luis';
+	/*$url = 'https://conference.eastus.cloudapp.azure.com/RocConf/serverapi.php?mode=affdexmerge&session_key=test-key-test&user=Luis';
 	$ch = curl_init($url);
 	curl_setopt($ch, CURLOPT_TIMEOUT, 5);
 	curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 	$data = curl_exec($ch);
 	curl_close($ch);
-	echo $data;
+	echo $data;*/
 ?>
 
 <html>
+
+<script>
+	var xhttp = new XMLHttpRequest();
+	xhttp.open("GET", "https://conference.eastus.cloudapp.azure.com/RocConf/serverapi.php?mode=affdexmerge&session_key=test-key-test&user=Luis", false);
+	xhttp.send();
+	console.log(xhttp.responseText);
+</script>
 
 
 </html>
