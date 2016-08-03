cdd856fe04deb8718f72cac24e7a58c67b645b32
jwhite37 <jwhite37@ur.rochester.edu>
2016-08-03 12:53:04 -0400

    Parser testing

--- AffdexParser.py
+++ AffdexParser.py
@@ -12,17 +12,47 @@ import pprint
 
 pp = pprint.PrettyPrinter(indent=2)
 
-client = MongoClient()
-database = client['rocconf']
-source_collection = database['affdexmerge']
+# Runnning through the raw data and extracting the stuff
+# we actually want to analyze
+def parse_raw_data(session_key, user):
+	client = MongoClient()
+	database = client['rocconf']
+	source_collection = database['affdexmerge']
 
-session_key = sys.argv[1]
-user = sys.argv[2]
+	document = source_collection.find_one({"session_key":session_key, "user":user})
 
-document = source_collection.find_one({"session_key":session_key, "user":user})
+	affdex_data = document["data"]
+	parsed_data = []
 
-affdex_data = document["data"]
+	pass_headers = 0
+	for row in affdex_data:
+		if pass_headers != 0:
+			sample = {}
+			sample["focus"] = row["focus"]
+			sample["engagement"] = row["engagement"]
+			sample["attention"] = row["attention"]
+			sample["surprise"] = row["surprise"]
+			sample["contempt"] = row["contempt"]
+			sample["joy"] = row["joy"]
+			sample["smirk"] = row["smirk"]
+			sample["relaxed"] = row["relaxed"]
+			sample["disappointed"] = row["disappointed"]
+			parsed_data.append(sample)
+		else:
+			pass_headers = 1
+			
+	return parsed_data
 
-for row in affdex_data:
-	pp.pprint(row)
-	break
+# Compute average statistics about a set of records
+def compute_averages(data, label):
+	return ""
+	
+#=======================================================
+# Main Caller
+#=======================================================
+if __name__ == "__main__":
+    parsed_data = parse_raw_data(sys.argv[1], sys.argv[2])
+    pp.pprint(parsed_data)
+	
+		
+		
