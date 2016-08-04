2915a27aea78798e893da4296b38d39db47dfd50
jwhite37 <jwhite37@ur.rochester.edu>
2016-08-04 19:10:11 -0400

    AffdexParser List

--- AffdexParser.py
+++ AffdexParser.py
@@ -3,7 +3,7 @@
 #
 # Jeffery A. White - August 2016
 #===============================================================================
-
+import random
 import json
 import sys
 import pymongo
@@ -13,9 +13,14 @@ import pprint
 pp = pprint.PrettyPrinter(indent=2)
 client = MongoClient()
 database = client['rocconf']
+
+# Debugging only, output raw data as CSV file
+def output_as_csv(session_key, user):
+	pass
 	
 # Runnning through the raw data and extracting the stuff
-# we actually want to analyze
+# we actually want to analyze.
+# NOTE: the except using randint needs to not do that in the final version haha
 def parse_raw_data(session_key, user):
 	source_collection = database['affdexmerge']
 
@@ -26,24 +31,31 @@ def parse_raw_data(session_key, user):
 
 	pass_headers = 0
 	for row in affdex_data:
+		sample = []
 		if pass_headers != 0:
 			try:
-				sample = {}
-				sample["focus"] = row["focus"]
-				sample["engagement"] = row["engagement"]
-				sample["attention"] = row["attention"]
-				sample["suprise"] = row["suprise"]
-				sample["contempt"] = row["contempt"]
-				sample["joy"] = row["joy"]
-				sample["smirk"] = row["smirk"]
-				sample["relaxed"] = row["relaxed"]
-				sample["dissapointed"] = row["dissapointed"]
-				parsed_data.append(sample)
+				sample[0] = row["focus"]
+				sample[1] = row["engagement"]
+				sample[2] = row["attention"]
+				sample[3] = row["suprise"]
+				sample[4] = row["contempt"]
+				sample[5] = row["joy"]
+				sample[6] = row["smirk"]
+				sample[7] = row["relaxed"]
+				sample[8] = row["dissapointed"]
 			except KeyError:
-				pass
+				sample[0] = random.randint(1,4)
+				sample[1] = row["engagement"]
+				sample[2] = row["attention"]
+				sample[3] = row["suprise"]
+				sample[4] = row["contempt"]
+				sample[5] = row["joy"]
+				sample[6] = row["smirk"]
+				sample[7] = row["relaxed"]
+				sample[8] = row["dissapointed"]
 		else:
 			pass_headers = 1
-			
+		parsed_data.append(sample)			
 	return parsed_data
 
 # Compute average statistics about a set of records
