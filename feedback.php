393bf708f57f49783c8f6989cbc6d5ff6e396b04
rzhao2 <rzhao2@u.rochester.edu>
2016-08-05 15:39:22 -0400

    console logs

--- feedback.php
+++ feedback.php
@@ -36,19 +36,19 @@
 	//interruption
 	var interruption = jscontent.interruption;
 	var defaultuser = Object.keys(interruption)[0]; //Luis interruption['Luis']
-	//console.log('interruption name { values }: ', Object.keys(interruption)[0], interruption[Object.keys(interruption)[0]]);
+	console.log('interruption name { values }: ', Object.keys(interruption)[0], interruption[Object.keys(interruption)[0]]);
 	var interrupted = interruption[defaultuser].interrupted;
-	// console.log('interrupted: ', interrupted);
+	console.log('interrupted: ', interrupted);
 
 	var interrupting = interruption[defaultuser].interrupting;
-	// console.log('interrupting: ', interrupting);
+	console.log('interrupting: ', interrupting);
 
 	//participation
 	var totalp = jscontent.participation['total'];
 
 	for (var key in jscontent.participation){
 		if(key=='total')
-			// console.log(key+'(sec) : ', jscontent.participation[key], 'percentage: ', 100*jscontent.participation[key]/total);
+			console.log(key+'(sec) : ', jscontent.participation[key], 'percentage: ', 100*jscontent.participation[key]/total);
 		// else 
 			delete jscontent.participation[key]; 
 	}	
