6b7325334152a8e12d2782490f4d0c628396cd53
rzhao2 <rzhao2@u.rochester.edu>
2016-08-08 13:02:26 -0400

    console count

--- graphs/public/main.js
+++ graphs/public/main.js
@@ -280,7 +280,7 @@ function loadTheRest(user) {
       transform:'t0,100'
     });
 
-    console.log("guests", guests[guests.to[count].guest]);
+    console.log("guests", count);
     var indivicolor = guests[guests.to[count].guest];
     var innercircle = s.circle(100,100, 68, 68).attr({
       fill: indivicolor,
