fadf532282f052c54b6de8f61bff5b67c6a4de7e
rzhao2 <rzhao2@u.rochester.edu>
2016-09-11 16:25:07 -0400

    logging sentiment

--- Real-Faces/static/js/AffdexJS/affdexheadtrack.js
+++ Real-Faces/static/js/AffdexJS/affdexheadtrack.js
@@ -85,6 +85,7 @@ detector.addEventListener("onImageResultsSuccess", function(faces, image, timest
     sentiment = JSON.stringify(faces[0].emotions, function(key, val) {
       return val.toFixed ? Number(val.toFixed(0)) : val;
     })
+    console.log(sentiment);
     /*log('#results', "Expressions: " + JSON.stringify(faces[0].expressions, function(key, val) {
       return val.toFixed ? Number(val.toFixed(0)) : val;
     }));
