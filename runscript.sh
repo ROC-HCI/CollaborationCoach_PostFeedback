788f83f00b534b994d9a7d4bd41facfef587ce99
jwhite37 <jwhite37@ur.rochester.edu>
2016-09-15 13:27:33 -0400

    Shell script update

--- runscript.sh
+++ runscript.sh
@@ -4,11 +4,11 @@ session_id=$1
 
 echo "Session Script Starting for: $session_id"
 
-#echo "Start - .webm Header Correction"
-#for file in Data/$session_id*.webm;
-#	do ffmpeg -i "$file" -c copy -fflags +genpts Data/fixed_$(basename "${file/.webm}").webm
-#done
-#echo "Finish - .webm Header Correction"
+echo "Start - .webm Header Correction"
+for file in Data/$session_id*.webm;
+	do ffmpeg -i "$file" -c copy -fflags +genpts Data/fixed_$(basename "${file/.webm}").webm
+done
+echo "Finish - .webm Header Correction"
 
 echo "Start - .wav Conversion"
 for file in Data/fixed_$session_id*.webm;
@@ -17,12 +17,7 @@ done
 echo "Finish - .wav Conversion"
 
 #CAN WE USE FFMPEG TO DO FLACC CONVERSION?
-
-#echo "Start - Affdex"
-#for file in Data/fixed_$session_id*.webm;
-#	do $HOME/build/video-demo/./video-demo -d $HOME/affdex-sdk/data -l cuzinniko@gmail.com.license -i $file
-#done
-#echo "Finish - Affdex"
+# YES WE CAN! THIS IS TO COME
 
 echo "Start - Praat"
 #./praat --run auto.praat Data/$session_id*
@@ -40,25 +35,11 @@ done
 python fileparser.py $argpath
 echo "Finish - Participation Analysis"
 
-#echo "Start - AffdexPlayerFocus Merge"
-#args=""
-#for file in Data/fixed_$session_id*.csv;
-#	do args=$(basename "${file/.csv}").json
-#	python AffdexPlayerFocusMerger.py "$file" "Data/$args"
-#done
-
-#echo "Finish - AffdexPlayerFocus Merge"
-
 #SETUP AVERAGES FOR AFFDEX FEATURES HERE
 
 #RUN BLUEMIX TONE ANALYSIS ON SUBMITTED SPEECH
 
-# NOT WORKING - License expired, need move to just tone analysis
-# potential here to boost speech recognition using this service as well
-# if we find it necessary.
-#echo "Start - BlueMix"
-#python ./bluemix_driver.py
-#echo "Finish - BlueMix"
+#POSSIBLE RUN BLUEMIX SPEECHRECOGNITION HERE
 
 echo "Session Script Terminated..."
 
