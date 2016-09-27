9d2491b980356ea09b5244c15cbdd8019182e391
jwhite37 <jwhite37@ur.rochester.edu>
2016-09-27 19:57:12 -0400

    Setting up the shell for a full test

--- runscript.sh
+++ runscript.sh
@@ -5,25 +5,28 @@ session_id=$1
 echo "Session Script Starting for: $session_id"
 
 echo "Start - .webm Header Correction"
-#for file in Data/$session_id*.webm;
-#	do ffmpeg -i "$file" -c copy -fflags +genpts Data/fixed_$(basename "${file/.webm}").webm
-#done
+for file in Data/$session_id*.webm;
+	do ffmpeg -i "$file" -c copy -fflags +genpts Data/fixed_$(basename "${file/.webm}").webm
+done
 echo "Finish - .webm Header Correction"
 
 echo "Start - .wav Conversion"
-#for file in Data/fixed_$session_id*.webm;
-#	do ffmpeg -i "$file" Data/$(basename "${file/.webm}").wav
-#done
+for file in Data/fixed_$session_id*.webm;
+	do ffmpeg -i "$file" Data/$(basename "${file/.webm}").wav
+done
 echo "Finish - .wav Conversion"
 
-#CAN WE USE FFMPEG TO DO FLACC CONVERSION?
-# YES WE CAN! THIS IS TO COME
+echo "Start - .flacc conversion"
+for file in Data/fixed_$session_id*.wav;
+	do ffmpeg -i "$file" Data/$(basename "${file/.wav}").flac
+done
+echo "End - .flacc conversion"
 
 echo "Start - Praat"
 #./praat --run auto.praat Data/$session_id*
-#for file in Data/fixed_$session_id*.wav;
-#	do ./praat --run auto.praat $file
-#done
+for file in Data/fixed_$session_id*.wav;
+	do ./praat --run auto.praat $file
+done
 echo "Finish - Praat"
 
 echo "Start - Participation Analysis"
@@ -36,6 +39,9 @@ python fileparser.py $argpath
 echo "Finish - Participation Analysis"
 
 #SETUP AVERAGES FOR AFFDEX FEATURES HERE
+echo "Start - average affdex features"
+python AffdexParser $session_id
+echo "Finish - average affdex features"
 
 #RUN BLUEMIX TONE ANALYSIS ON SUBMITTED SPEECH
 
