e917bfdbd0341284b33f28f7ba5460096c55c95d
jwhite37 <jwhite37@ur.rochester.edu>
2016-10-02 13:33:24 -0400

    Shell update for test

--- runscript.sh
+++ runscript.sh
@@ -5,15 +5,15 @@ session_id=$1
 echo "Session Script Starting for: $session_id"
 
 echo "Start - .webm Header Correction"
-for file in Data/$session_id*.webm;
-	do ffmpeg -i "$file" -c copy -fflags +genpts Data/fixed_$(basename "${file/.webm}").webm
-done
+#for file in Data/$session_id*.webm;
+#	do ffmpeg -i "$file" -c copy -fflags +genpts Data/fixed_$(basename "${file/.webm}").webm
+#done
 echo "Finish - .webm Header Correction"
 
 echo "Start - .wav Conversion"
-for file in Data/fixed_$session_id*.webm;
-	do ffmpeg -i "$file" Data/$(basename "${file/.webm}").wav
-done
+#for file in Data/fixed_$session_id*.webm;
+#	do ffmpeg -i "$file" Data/$(basename "${file/.webm}").wav
+#done
 echo "Finish - .wav Conversion"
 
 echo "Start - .flacc conversion"
@@ -24,28 +24,31 @@ echo "End - .flacc conversion"
 
 echo "Start - Praat"
 #./praat --run auto.praat Data/$session_id*
-for file in Data/fixed_$session_id*.wav;
-	do ./praat --run auto.praat $file
-done
+#for file in Data/fixed_$session_id*.wav;
+#	do ./praat --run auto.praat $file
+#done
 echo "Finish - Praat"
 
 echo "Start - Participation Analysis"
-argpath=""
-for i in Data/fixed_$session_id*.wav.TextGrid;
-	do argpath="$argpath $i"
-done
+#argpath=""
+#for i in Data/fixed_$session_id*.wav.TextGrid;
+#	do argpath="$argpath $i"
+#done
 
-python fileparser.py $argpath
+#python fileparser.py $argpath
 echo "Finish - Participation Analysis"
 
-#SETUP AVERAGES FOR AFFDEX FEATURES HERE
 echo "Start - average affdex features"
-python AffdexParser $session_id
+#python AffdexParser $session_id
 echo "Finish - average affdex features"
 
-#RUN BLUEMIX TONE ANALYSIS ON SUBMITTED SPEECH
+echo "Start - Bluemix Speech Recognition"
+for file in Data/fixed_$session_id*.flac;
+	do python BluemixSpeech $file
+done
+echo "Finish - Bluemix Speech Recognition"
 
-#POSSIBLE RUN BLUEMIX SPEECHRECOGNITION HERE
+#RUN BLUEMIX TONE ANALYSIS ON RECOGNIZED SPEECH
 
 echo "Session Script Terminated..."
 
