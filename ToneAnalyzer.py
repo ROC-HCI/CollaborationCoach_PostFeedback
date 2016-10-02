39f38b09af95b65bffa15342dd866053aa0cdb75
jwhite37 <jwhite37@ur.rochester.edu>
2016-10-02 16:00:59 -0400

    indents...

--- ToneAnalyzer.py
+++ ToneAnalyzer.py
@@ -37,48 +37,45 @@ IBM_TONE_PASSWORD = "FOG5rXrhNK0J"
 # Get the text transcript for this user from MongoDB
 #==================================================================================
 def get_transcript(session_id, user_id):
-    db_data = read_collection.find({"session_key":session_id,"user":user_id})
+	db_data = read_collection.find({"session_key":session_id,"user":user_id})
 	item = db_data[1]
 	
 	transcript_text = item["transcript"]
-    
-    return transcript_text
-    
+
+	return transcript_text
+
 #==================================================================================
 # Run this on a text based transcript to obtain tone analysis, add this
 # analysis data to the tone collection
 #==================================================================================
 def process_tone(transcript_text, session_id, user_id):
-    url = "https://gateway.watsonplatform.net/tone-analyzer/api/v3/tone?{0}" . format(urlencode({
-            "version": "2016-05-19",
-            "text": transcript_text
-        }))
-        
-    request = Request(url, headers = {"Content-Type": "application/json"})
-        
-    if hasattr("", "encode"):
-        authorization_value = base64.standard_b64encode("{0}:{1}".format(IBM_TONE_USERNAME, IBM_TONE_PASSWORD).encode("utf-8")).decode("utf-8")
-    else:
-        authorization_value = base64.standard_b64encode("{0}:{1}".format(IBM_TONE_USERNAME, IBM_TONE_PASSWORD))
-    
-    request.add_header("Authorization", "Basic {0}" . format(authorization_value))
-    
-    try:
-        response = urlopen(request)
-    except HTTPError as e:
-        raise RequestError("tone analysis request failed: {0}".format(getattr(e, "reason", "status {0}".format(e.code)))) # use getattr to be compatible with Python 2.6
-    except URLError as e:
-        raise RequestError("tone analysis connection failed: {0}".format(e.reason))
-        
-    response_text = response.read().decode("utf-8")
-    result = json.loads(response_text)
-    
-    final_dict = {}
-    final_dict["session_key"] = session_id
-    final_dict["user"] = user_id
-    final_dict["data"] = result
-    
-    pp.pprint(write_collection.insert_one(final_dict).inserted_id)
+	url = "https://gateway.watsonplatform.net/tone-analyzer/api/v3/tone?{0}" . format(urlencode({"version": "2016-05-19","text": transcript_text}))
+
+	request = Request(url, headers = {"Content-Type": "application/json"})
+
+	if hasattr("", "encode"):
+		authorization_value = base64.standard_b64encode("{0}:{1}".format(IBM_TONE_USERNAME, IBM_TONE_PASSWORD).encode("utf-8")).decode("utf-8")
+	else:
+		authorization_value = base64.standard_b64encode("{0}:{1}".format(IBM_TONE_USERNAME, IBM_TONE_PASSWORD))
+
+	request.add_header("Authorization", "Basic {0}" . format(authorization_value))
+
+	try:
+		response = urlopen(request)
+	except HTTPError as e:
+		raise RequestError("tone analysis request failed: {0}".format(getattr(e, "reason", "status {0}".format(e.code)))) # use getattr to be compatible with Python 2.6
+	except URLError as e:
+		raise RequestError("tone analysis connection failed: {0}".format(e.reason))
+
+	response_text = response.read().decode("utf-8")
+	result = json.loads(response_text)
+
+	final_dict = {}
+	final_dict["session_key"] = session_id
+	final_dict["user"] = user_id
+	final_dict["data"] = result
+
+	pp.pprint(write_collection.insert_one(final_dict).inserted_id)
 
 #=======================================================
 # Main Caller
@@ -88,5 +85,5 @@ if __name__ == "__main__":
 	session_key = final[1]
 	user_id = final[2]
 	
-    transcript = get_transcript(session_key, user)
-    process_tone(transcript)
+	transcript = get_transcript(session_key, user)
+	process_tone(transcript)
