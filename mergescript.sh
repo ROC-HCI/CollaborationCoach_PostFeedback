e54530ce45724f633a591f4d05636722627bb661
rzhao2 <rzhao2@u.rochester.edu>
2016-10-04 00:40:22 -0400

    update script

--- mergescript.sh
+++ mergescript.sh
@@ -1,5 +1,5 @@
 ffmpeg
-	-i Data/fixed_bd3f7530-89b4-11e6-9414-799760e2546b_vivian_1.webm -i fixed_bd3f7530-89b4-11e6-9414-799760e2546b_ssmumu_1.webm -i fixed_bd3f7530-89b4-11e6-9414-799760e2546b_Ru_1.webm	
+	-i Data/fixed_bd3f7530-89b4-11e6-9414-799760e2546b_vivian_1.webm -i Data/fixed_bd3f7530-89b4-11e6-9414-799760e2546b_ssmumu_1.webm -i Data/fixed_bd3f7530-89b4-11e6-9414-799760e2546b_Ru_1.webm	
 	-filter_complex "
 		nullsrc=size=640x480 [base];
 		[0:v] setpts=PTS-STARTPTS, scale=320x240 [upperleft];
@@ -9,4 +9,4 @@ ffmpeg
 		[tmp1][upperright] overlay=shortest=1:x=320 [tmp2];
 		[tmp2][lowerleft] overlay=shortest=1:y=240 [tmp3];
 	"
-	-c:v libx264 gamesession.webm
+	-c:v libx264 Data/gamesession.webm
