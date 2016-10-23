164da01e9d70dac1a18f01d5563501d70fcbd144
Yichen Lu <yichen_0@hotmail.com>
2016-10-23 17:05:30 -0400

    Add files via upload

---
+++ chat/public/talktimeDialogue.js
@@ -0,0 +1,12 @@
+var json = 
+[
+    {
+        "title": "participation_2good_withGraph","tags": "","body": "Strong participation! You participated the most of everyone in the group.\\nTeam management experts have found that equal participation in collaborative-decision making is a sign of a healthy team. Wanna know more about it?\\n\n[[Sure|participation_4yes]]\n[[No Thanks|turntaking_1]]","position": {"x": 382,"y": 129},"colorID": 0
+    },
+    {
+        "title": "participation_2bad_withGraph","tags": "","body": "You talked relatively less during the discussion.\\nTeam management experts have found that equal participation in collaborative-decision making is a sign of a healthy team. Wanna know more about it?\\n\n[[Sure|participation_3extra]]\n[[No Thanks|turntaking_1]]\n","position": {"x": 382,"y": 429},"colorID": 0
+    },
+    {
+        "title": "participation_3extra","tags": "","body": "Extra info link for participation\\n\n[[Got it!|turntaking_1]]","position": {"x": 962,"y": 58},"colorID": 0},{"title": "turntaking_1","tags": "","body": "Now moving on towards turntaking","position": {"x": 1372,"y": 285},"colorID": 0
+    }
+]
