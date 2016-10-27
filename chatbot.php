659b4dcc50ed6a4857138275ecbc35e2858a26ee
rzhao2 <rzhao2@u.rochester.edu>
2016-10-27 05:36:43 -0400

    ya

--- chatbot.php
+++ chatbot.php
@@ -108,6 +108,9 @@
         	var message = $('#messages');
         	console.log('what', line, dialog[line.trim()]);
 
+        	//testing graph make
+        	createGraph();
+
 	        setTimeout(function(){
 	        	message.append(
 	        		new item("Roboto", ButtonFix(dialog[line.trim()]+"")
