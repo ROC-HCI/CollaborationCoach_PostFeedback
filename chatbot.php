81247b1984abd35f0fd6dcfa08af1420faaa3786
rzhao2 <rzhao2@u.rochester.edu>
2016-10-27 04:59:53 -0400

    remove mess

--- chatbot.php
+++ chatbot.php
@@ -44,79 +44,33 @@
 					<!-- actual feedback diagram -->
 					<div id="graphContainer" class="inner-contain-graph">
 						<!-- some kind of navigation instruction -->
-						<div id="accordion">
-  <h3>Section 1</h3>
-  <div>
-    <p>
-    Mauris mauris ante, blandit et, ultrices a, suscipit eget, quam. Integer
-    ut neque. Vivamus nisi metus, molestie vel, gravida in, condimentum sit
-    amet, nunc. Nam a nibh. Donec suscipit eros. Nam mi. Proin viverra leo ut
-    odio. Curabitur malesuada. Vestibulum a velit eu ante scelerisque vulputate.
-    </p>
-  </div>
-  <h3>Section 2</h3>
-  <div>
-    <p>
-    Sed non urna. Donec et ante. Phasellus eu ligula. Vestibulum sit amet
-    purus. Vivamus hendrerit, dolor at aliquet laoreet, mauris turpis porttitor
-    velit, faucibus interdum tellus libero ac justo. Vivamus non quam. In
-    suscipit faucibus urna.
-    </p>
-  </div>
-  <h3>Section 3</h3>
-  <div>
-    <p>
-    Nam enim risus, molestie et, porta ac, aliquam ac, risus. Quisque lobortis.
-    Phasellus pellentesque purus in massa. Aenean in pede. Phasellus ac libero
-    ac tellus pellentesque semper. Sed ac felis. Sed commodo, magna quis
-    lacinia ornare, quam ante aliquam nisi, eu iaculis leo purus venenatis dui.
-    </p>
-    <ul>
-      <li>List item one</li>
-      <li>List item two</li>
-      <li>List item three</li>
-    </ul>
-  </div>
-  <h3>Section 4</h3>
-  <div>
-    <p>
-    Cras dictum. Pellentesque habitant morbi tristique senectus et netus
-    et malesuada fames ac turpis egestas. Vestibulum ante ipsum primis in
-    faucibus orci luctus et ultrices posuere cubilia Curae; Aenean lacinia
-    mauris vel est.
-    </p>
-    <p>
-    Suspendisse eu nisl. Nullam ut libero. Integer dignissim consequat lectus.
-    Class aptent taciti sociosqu ad litora torquent per conubia nostra, per
-    inceptos himenaeos.
-    </p>
-  </div>
-</div>
  
 			 		<div id="accordion">
 
-			 		<d1iv class="inner-contain1">
-			        <h1>Interruption</h1>
-			        <div class="outer">
-			          <p><span class="chart-label" id="chart1"></span>
-			          </p>
-			          <p>By You</p>
-			        </div>
-			        <div class="outer">
-			          <p><span class="chart-label" id="chart2"></span>
-			          </p>
-			          <p>By Others</p>
-			        </div>
-			      </div>
-			      <div class="inner-contain2">
-			        <div class="outer2">
-			          <h1>Participation</h1>
-			          <canvas class="chart" id="chart3" data-value="0" data-speaker=""></canvas>
-			        </div>
-			      </div>
-			      <h1>Turn Taking</h1>
-			      <div class="inner-contain3" id="chart4" data-value="0" data-user=""></div>
-			      <div>
+				 		<div class="inner-contain1">
+				        <h1>Interruption</h1>
+				        <div class="outer">
+				          <p><span class="chart-label" id="chart1"></span>
+				          </p>
+				          <p>By You</p>
+				        </div>
+				        <div class="outer">
+				          <p><span class="chart-label" id="chart2"></span>
+				          </p>
+				          <p>By Others</p>
+				        </div>
+				        </div>
+				        
+						<div class="inner-contain2">
+						<div class="outer2">
+						  <h1>Participation</h1>
+						  <canvas class="chart" id="chart3" data-value="0" data-speaker=""></canvas>
+						</div>
+						</div>
+						<h1>Turn Taking</h1>
+						<div class="inner-contain3" id="chart4" data-value="0" data-user=""></div>
+	
+			      	</div>
 
 					</div>
 				</div>
