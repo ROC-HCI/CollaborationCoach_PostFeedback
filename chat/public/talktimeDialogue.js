371ec8132e9c8bbdf9c4eb987e6e18d6ac21cea8
rzhao2 <rzhao2@u.rochester.edu>
2016-10-28 19:35:41 -0400

    newest version of dialogue tree

--- chat/public/talktimeDialogue.js
+++ chat/public/talktimeDialogue.js
@@ -1,12 +1,13 @@
 var json = 
 [
-    {
-        "title": "participation_2good_withGraph","tags": "","body": "Strong participation! You participated the most of everyone in the group.\\nTeam management experts have found that equal participation in collaborative-decision making is a sign of a healthy team. Wanna know more about it?\\n\n[[Sure|participation_4yes]]\n[[No Thanks|turntaking_1]]","position": {"x": 382,"y": 129},"colorID": 0
-    },
-    {
-        "title": "participation_2bad_withGraph","tags": "","body": "You talked relatively less during the discussion.\\nTeam management experts have found that equal participation in collaborative-decision making is a sign of a healthy team. Wanna know more about it?\\n\n[[Sure|participation_3extra]]\n[[No Thanks|turntaking_1]]\n","position": {"x": 382,"y": 429},"colorID": 0
-    },
-    {
-        "title": "participation_3extra","tags": "","body": "Extra info link for participation\\n\n[[Got it!|turntaking_1]]","position": {"x": 962,"y": 58},"colorID": 0},{"title": "turntaking_1","tags": "","body": "Now moving on towards turntaking","position": {"x": 1372,"y": 285},"colorID": 0
-    }
+    {"title": "intro_1","tags": "","body": "Hi, I'm your personal feedback assistant!\\nI've analyzed your performance of the previous session. Let's walk you through it.\n[[Sounds great!|participation_1]]","position": {"x": 740,"y": 378},"colorID": 0},
+    
+    {"title": "participation_1","tags": "","body": "First, let's move to participation\n[[Okay1|participation_2]]","position": {"x": 319,"y": 328},"colorID": 0},
+    {"title": "participation_2","tags": "1","body": "\n[[go to|participation_3]]","position": {"x": 676,"y": 209},"colorID": 1},
+    {"title": "participation_3","tags": "","body": "Wanna know more about it?\n[[Sure|participation_4]]\n[[No Thanks|overlap_1]]","position": {"x": 674,"y": 479},"colorID": 1},
+    {"title": "participation_4","tags": "","body": "Extra info link for participation\n[[Got it!|overlap_1]]","position": {"x": 1090,"y": 123},"colorID": 1},
+   
+    {"title": "overlap_1","tags": "1","body": "\n[[next|overlap_2]]","position": {"x": 294,"y": 316},"colorID": 0},
+    {"title": "overlap_2","tags": "","body": "overlaps aren't always a bad thing, they can add to a conversation. But they can also take away from one. Just something to keep in mind.\n[[Got it|finish_1]]","position": {"x": 617,"y": 159},"colorID": 0},
+    {"title": "finish_1","tags": "","body": "This is all, good job expressing!\\nHope you have a great team discussion next time.\n[[Thanks, bye!|]]","position": {"x": 1001,"y": 154},"colorID": 0 }
 ]
