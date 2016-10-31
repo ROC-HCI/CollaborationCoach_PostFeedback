81dec169b4633c3f686cc331fac6ce23256f74ab
jwhite37 <jwhite37@ur.rochester.edu>
2016-10-31 18:48:34 -0400

    shell update

--- runscript.sh
+++ runscript.sh
@@ -4,29 +4,29 @@ session_id=$1
 
 echo "Session Script Starting for: $session_id"
 
-echo "Start - .webm Header Correction"
-for file in Data/$session_id*.webm;
-	do ffmpeg -i "$file" -c copy -fflags +genpts Data/fixed_$(basename "${file/.webm}").webm
-done
-echo "Finish - .webm Header Correction"
+#echo "Start - .webm Header Correction"
+#for file in Data/$session_id*.webm;
+#	do ffmpeg -i "$file" -c copy -fflags +genpts Data/fixed_$(basename "${file/.webm}").webm
+#done
+#echo "Finish - .webm Header Correction"
 
-echo "Start - .wav Conversion"
-for file in Data/fixed_$session_id*.webm;
-	do ffmpeg -i "$file" Data/$(basename "${file/.webm}").wav
-done
-echo "Finish - .wav Conversion"
+#echo "Start - .wav Conversion"
+#for file in Data/fixed_$session_id*.webm;
+#	do ffmpeg -i "$file" Data/$(basename "${file/.webm}").wav
+#done
+#echo "Finish - .wav Conversion"
 
-echo "Start - .flacc conversion"
-for file in Data/fixed_$session_id*.wav;
-	do ffmpeg -i "$file" Data/$(basename "${file/.wav}").flac
-done
-echo "End - .flacc conversion"
+#echo "Start - .flacc conversion"
+#for file in Data/fixed_$session_id*.wav;
+#	do ffmpeg -i "$file" Data/$(basename "${file/.wav}").flac
+#done
+#echo "End - .flacc conversion"
 
-echo "Start - Praat"
-for file in Data/fixed_$session_id*.wav;
-	do ./praat --run auto.praat $file
-done
-echo "Finish - Praat"
+#echo "Start - Praat"
+#for file in Data/fixed_$session_id*.wav;
+#	do ./praat --run auto.praat $file
+#done
+#echo "Finish - Praat"
 
 echo "Start - Participation Analysis"
 argpath=""
@@ -37,18 +37,18 @@ done
 python fileparser.py $argpath
 echo "Finish - Participation Analysis"
 
-echo "Start - average affdex features"
-python AffdexParser.py $session_id
-echo "Finish - average affdex features"
+#echo "Start - average affdex features"
+#python AffdexParser.py $session_id
+#echo "Finish - average affdex features"
 
-echo "Start - shared affdex features"
-python SharedAffdexDetection.py $session_id
-echo "Finish - shared affdex features"
+#echo "Start - shared affdex features"
+#python SharedAffdexDetection.py $session_id
+#echo "Finish - shared affdex features"
 
-echo "Start - Bluemix Tone Analysis - Google Results"
-for file in Data/fixed_$session_id*.flac;
-	do python ToneAnalyzer.py $file "google"
-done
+#echo "Start - Bluemix Tone Analysis - Google Results"
+#for file in Data/fixed_$session_id*.flac;
+#	do python ToneAnalyzer.py $file "google"
+#done
 echo "Finish - Bluemix Tone Analysis - Google Results"
 
 #echo "Start - Bluemix Speech Recognition"
