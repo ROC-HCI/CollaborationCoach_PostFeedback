731489c63f099635f61ca2ac734cbbd3dd2fd3e0
rzhao2 <rzhao2@u.rochester.edu>
2016-10-31 15:03:03 -0400

    update dialogue tree from samiha

--- chat/public/talktimeDialogue.js
+++ chat/public/talktimeDialogue.js
@@ -1,102 +1,184 @@
 var json = 
 [
     {
-    	"title": "intro_1",
-    	"tags": "",
-    	"body": "Hi, I'm your personal feedback assistant!\\nI've analyzed your performance of the previous session. Let's walk you through it.\n[[Sounds great!|participation_1]]",
-    	"position": {"x": 740,"y": 378},"colorID": 0
+        "title": "intro_1",
+        "tags": "",
+        "body": "Hi, I'm Roboto, your personal feedback assistant!\\nI've analyzed your performance of the previous session. Let's walk through it, shall we?\n[[Sure, let's|participation_1]]",
+        "position": {
+            "x": 27,
+            "y": 96
+        },
+        "colorID": 0
     },
     {
-    	"title": "participation_1",
-    	"tags": "",
-    	"body": "First, let's move to participation\n[[Okay1|participation_2]]",
-    	"position": {"x": 319,"y": 328},"colorID": 0
+        "title": "participation_1",
+        "tags": "",
+        "body": "First, moving towards talktime as participation.\n[[Okay|participation_2]]",
+        "position": {
+            "x": 301,
+            "y": 96
+        },
+        "colorID": 0
     },
     {
-    	"title": "participation_2",
-    	"tags": "1",
-    	"body": "\n[[go to|participation_3]]",
-    	"position": {"x": 676,"y": 209},"colorID": 1
+        "title": "participation_2",
+        "tags": "1",
+        "body": "\n[[Sure|participation_4]]\n[[No Thanks|overlap_1]]",
+        "position": {
+            "x": 572,
+            "y": 99
+        },
+        "colorID": 0
     },
     {
-    	"title": "participation_3",
-    	"tags": "",
-    	"body": "Wanna know more about it?\n[[Sure|participation_4]]\n[[No Thanks|overlap_1]]",
-    	"position": {"x": 674,"y": 479},"colorID": 1},
+        "title": "participation_4",
+        "tags": "",
+        "body": "Team management experts have found that, equal participation help the participants resolve any confusion and shed light on the discussion from different perspectives.\\nAlongside, listening to teammates' viewpoints is also valuable.\\\\nRemember, balance is the key!\n[[Got it!|overlap_1]]",
+        "position": {
+            "x": 839,
+            "y": 99
+        },
+        "colorID": 0
+    },
     {
-    	"title": "participation_4",
-    	"tags": "",
-    	"body": "Extra info link for participation\\n\n[[Got it!|overlap_1]]",
-    	"position": {"x": 962,"y": 58},"colorID": 0},
+        "title": "overlap_1",
+        "tags": "1",
+        "body": "Now moving onto interruption.\\nOn average you overlapped with others for ______ seconds and about ___ times\n[[what's this mean?|overlap_2]]",
+        "position": {
+            "x": -47,
+            "y": 140
+        },
+        "colorID": 0
+    },
     {
-		"title": "overlap_1",
-		"tags": "",
-		"body": "Now moving onto overlaps.\\nOn average you overlapped with others for ______ seconds and about ___ times [[what's this mean?|overlap_2]]",
-		"position": {
-			"x": -47,
-			"y": 140
-		},
-		"colorID": 0
-	},
-	{
-		"title": "overlap_2",
-		"tags": "",
-		"body": "Overlaps are when you are speaking at the same time as another person. \\n They can add or take away from a conversation. Just something to keep in mind.[[Good to know|turntaking_1]] [[Explain more|overlap_3]]",
-		"position": {
-			"x": 179,
-			"y": 141
-		},
-		"colorID": 0
-	},
-	{
-		"title": "turntaking_1",
-		"tags": "",
-		"body": "While we're on overlaps, they have a lot to do with turn-taking. They can help decide who speaks next.\n [[Interesting|turntaking_2]]",
-		"position": {
-			"x": -36,
-			"y": 395
-		},
-		"colorID": 0
-	},
-	{
-		"title": "overlap_3",
-		"tags": "",
-		"body": "Overlaps can sometimes be called interruptions, when you break someone off before they are done.\\nBut they are also good for showing that you are engaged in the conversation.[[Cool|turntaking_1]]",
-		"position": {
-			"x": 393,
-			"y": 138
-		},
-		"colorID": 0
-	},
-	{
-		"title": "turntaking_2",
-		"tags": "turntaking_graph",
-		"body": "[[neutral|turntaking_3]][[good|turntaking_4]]",
-		"position": {
-			"x": 191,
-			"y": 399
-		},
-		"colorID": 0
-	},
-	{
-		"title": "turntaking_3",
-		"tags": "",
-		"body": "You spoke after ____ the most and ___ the least. [[Okay|finish_1]]",
-		"position": {
-			"x": 636,
-			"y": 413
-		},
-		"colorID": 0
-	},
-	{
-		"title": "turntaking_4",
-		"tags": "",
-		"body": "You spoke after everyone about equally. Looks like it was an involved conversation [[Yup!|finish_1]]",
-		"position": {
-			"x": 643,
-			"y": 173
-		},
-		"colorID": 0
-	},
-	{"title": "finish_1","tags": "","body": "This is all, good job expressing!\\nHope you have a great team discussion next time.\n[[Thanks, bye!|]]","position": {"x": 1001,"y": 154},"colorID": 0 }
+        "title": "overlap_2",
+        "tags": "",
+        "body": "Overlaps are when you are speaking at the same time as another person. \\n They can add or take away from a conversation. Just something to keep in mind.[[Good to know|turntaking_1]] [[Explain more|overlap_3]]",
+        "position": {
+            "x": 179,
+            "y": 141
+        },
+        "colorID": 0
+    },
+    {
+        "title": "turntaking_1",
+        "tags": "",
+        "body": "While we're on overlaps, they have a lot to do with turn-taking. They can help decide who speaks next.\n [[Interesting|turntaking_2]]",
+        "position": {
+            "x": -36,
+            "y": 395
+        },
+        "colorID": 0
+    },
+    {
+        "title": "overlap_3",
+        "tags": "",
+        "body": "Overlaps can sometimes be called interruptions, when you break someone off before they are done.\\nBut they are also good for showing that you are engaged in the conversation.[[Cool|turntaking_1]]",
+        "position": {
+            "x": 393,
+            "y": 138
+        },
+        "colorID": 0
+    },
+    {
+        "title": "turntaking_2",
+        "tags": "1",
+        "body": "\n[[neutral|turntaking_3]]\n[[good|turntaking_4]]",
+        "position": {
+            "x": 191,
+            "y": 399
+        },
+        "colorID": 0
+    },
+    {
+        "title": "turntaking_3",
+        "tags": "",
+        "body": "You spoke after ____ the most and ___ the least. [[Okay|finish_1]]",
+        "position": {
+            "x": 636,
+            "y": 413
+        },
+        "colorID": 0
+    },
+    {
+        "title": "turntaking_4",
+        "tags": "",
+        "body": "You spoke after everyone about equally. Looks like it was an involved conversation [[Yup!|emotion_1]]",
+        "position": {
+            "x": 643,
+            "y": 173
+        },
+        "colorID": 0
+    },
+
+    {
+        "title": "emotion_1",
+        "tags": "",
+        "body": "I think now is a good time to talk about emotion. These discrete short-term feelings can both derive from and affect group discussion.\n[[Show mine|emotion_2]]",
+        "position": {
+            "x": 28,
+            "y": 418
+        },
+        "colorID": 0
+    },
+    {
+        "title": "emotion_2",
+        "tags": "1",
+        "body": "\n[[Seems about right|emotion_3]]\n[[Is that so?|emotion_3]]",
+        "position": {
+            "x": 291,
+            "y": 418
+        },
+        "colorID": 0
+    },
+    {
+        "title": "emotion_3",
+        "tags": "1",
+        "body": "Building onto that, now I break down your individual sentiments which are engaged, joyous, relaxed and negative. You can also see how the group sentiment went.\n[[Why should I care?|emotion_4]]",
+        "position": {
+            "x": 576,
+            "y": 415
+        },
+        "colorID": 0
+    },
+    {
+        "title": "emotion_4",
+        "tags": "",
+        "body": "Positive sentiments and emotions can flow cooperativeness within the group, whereas negative ones can reduce the group performance. These can even affect the bonds within the group members.\\n On this note, wanna see how much you bonded with the group?\n[[Of course!|sharedsmile_1]]",
+        "position": {
+            "x": 860,
+            "y": 413
+        },
+        "colorID": 0
+    },
+    {
+        "title": "sharedsmile_1",
+        "tags": "",
+        "body": "Great! So first let me tell you what it represents.\\nSmile is one of the parameters for passing assurance and building bonds. Shared smile is how much one person smiled with another for the same period - which can tell a lot about group dynamics.\\nHere goes your performance analysisâ€¦\n[[View|sharedsmile_2]]",
+        "position": {
+            "x": 32,
+            "y": 702
+        },
+        "colorID": 0
+    },
+    {
+        "title": "sharedsmile_2",
+        "tags": "1",
+        "body": "\n[[Sweet!|finish_1]]",
+        "position": {
+            "x": 294,
+            "y": 701
+        },
+        "colorID": 0
+    },
+    {
+        "title": "finish_1",
+        "tags": "",
+        "body": "This is all I have. Good job expressing!\\nAll these feedback might help you in your future group meetings.\\nHope you have a great team discussion next time!\n[[Thanks, bye!|]]",
+        "position": {
+            "x": 589,
+            "y": 704
+        },
+        "colorID": 0
+    }
 ]
