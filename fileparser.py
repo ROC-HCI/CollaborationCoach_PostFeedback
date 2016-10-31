c5340e958bdd0c159f79a0173336ac60724e1f44
jwhite37 <jwhite37@ur.rochester.edu>
2016-10-31 19:12:47 -0400

    test threshold

--- fileparser.py
+++ fileparser.py
@@ -96,10 +96,10 @@ def readFile(file):
 def overlap(a,b):	
 	print float(a.xmax)
 	# Filtering for duration of the speech
-	if((float(a.xmax) - float(a.xmin)) < 0.5):
+	if((float(a.xmax) - float(a.xmin)) < 0.75):
 		return False
 		
-	if((float(b.xmax) - float(b.xmin)) < 0.5):
+	if((float(b.xmax) - float(b.xmin)) < 0.75):
 		return False
 
 	# Is a contained within b?
