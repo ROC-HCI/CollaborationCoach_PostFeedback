27a6e1b9155cbbcb53a3876a1dded2cf8e9d2373
ssmumu <samiha.mumu@gmail.com>
2016-11-05 03:53:09 -0400

    changing part-turn

--- chatbot.php
+++ chatbot.php
@@ -104,7 +104,7 @@
 		});
 	
 		var countType = 0;
-		var graphType = ["interruption", "participation", "turntaking", "valence", "attitude", "smilesharing"];
+		var graphType = ["interruption", "turntaking", "participation", "valence", "attitude", "smilesharing"];
 	
         function gotoObject(object){
         	document.getElementById('message-option').innerHTML="";
