af6cb76295b208d445e459ce805d54d82efea1d6
ssmumu <samiha.mumu@gmail.com>
2016-11-04 23:45:29 -0400

    testing 2nd graph

--- graphs/public/main.js
+++ graphs/public/main.js
@@ -8,7 +8,7 @@ function make_Graph(type){
       var ss = Snap('#svgout');
       var g = ss.g();
       var image = g.image('./graphs/public/svg/overlap.svg',5,5,100,100);
-      myNumber('#chart1',i1);
+    //  myNumber('#chart1',i1);
       break;
     case "participation":
       ChartJS('#chart3');
