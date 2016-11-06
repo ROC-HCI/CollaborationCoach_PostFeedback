e980cc012369243dd6ac2266f01142bedc165a14
jwhite37 <jwhite37@ur.rochester.edu>
2016-11-06 18:43:27 -0500

    update overlaps

--- graphs/public/main.js
+++ graphs/public/main.js
@@ -3,8 +3,10 @@ function make_Graph(type){
   {
     case "interruption":
       //animate numbers - interruption
-      myNumber('#chart1',i1);
-      myNumber('#chart2',i2);
+	  $('#svg_41').val(i1);
+	  $('#svg_42').val(i2);)
+      //myNumber('#chart1',i1);
+      //myNumber('#chart2',i2);
    //   var ss = Snap("#guest0");
   //  var g = ss.g();
  //   var image = g.image("./graphs/public/svg/overlap.svg",0,0,200,200);
