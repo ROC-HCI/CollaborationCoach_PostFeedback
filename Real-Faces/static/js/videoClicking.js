498fbef432ea6a56eec78b78edc8e646ae3c907d
LudwigNova <lnova@u.rochester.edu>
2016-11-06 15:35:15 -0500

    videoClicking changes
    Got rid of the popup that occurs if the mouse hovers over the space
    between remotesVideos elements.

--- Real-Faces/static/js/videoClicking.js
+++ Real-Faces/static/js/videoClicking.js
@@ -1,4 +1,3 @@
-// $(document).ready(function(e){
 	var remoteVideo = document.getElementById("remotesVideos");
 	var bigVid = document.getElementById("bigFeed");
 
@@ -7,27 +6,7 @@
 			bigVid.innerHTML = "";
 			$(event.target).clone().appendTo(bigVid);
 		}
-		else{
-			window.alert("parent");
-		}
+		// else{
+		// 	window.alert("parent");
+		// }
 	});
-
-// $("#remotesVideos").on("click", function(event){
-// 		if (event.target != this){
-// 			bigVid.innerHTML = "";
-// 			$(event.target).clone().appendTo(bigVid);
-// 		}
-// 		else{
-// 			window.alert("parent");
-// 		}
-// 	});
-// });
-
-// function enlargeVideoFeed(){
-// 	var remoteVideo = document.getElementById("remotesVideos");
-// 	var bigVid = document.getElementById("bigFeed");
-
-
-// 	bigVid.innerHTML = "";
-// 	bigVid.innerHTML = remoteVideo.innerHTML;
-// }
