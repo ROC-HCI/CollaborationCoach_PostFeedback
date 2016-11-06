8cd74ba757bb9b6d0ae468027bc9dc33bc07c41d
rzhao2 <rzhao2@u.rochester.edu>
2016-11-06 13:06:30 -0500

    overlap svg

--- graphs/public/main.js
+++ graphs/public/main.js
@@ -300,8 +300,7 @@ var chart = new Chart($(id),{
         labels: i3speakernew,
         datasets: [{
             data: i3data,
-            backgroundColor: colorpalette,
-            label: "test"
+            backgroundColor: colorpalette
         }]
     },
     options: {
