d91dc4ffe044deb25d749ad41f46f65fc2bca5be
vivss <vli4@u.rochester.edu>
2016-11-06 12:34:13 -0500

    Edited dialogue wording and grammer

--- chat/public/talktimeDialogue.js
+++ chat/public/talktimeDialogue.js
@@ -3,7 +3,7 @@ var json =
     {
         "title": "intro_1",
         "tags": "",
-        "body": "Hi, I'm Roboto, your personal feedback assistant!\\nI've analyzed your performance of the previous session. Let's walk through it, shall we?\n[[Sure, let's do this!|participation_1]]",
+        "body": "Hi, I'm Roboto, your personal feedback assistant!\\nI've analyzed your performance from the previous session. Let's walk through it, shall we?\n[[Sure, let's do this!|participation_1]]",
         "position": {
             "x": 27,
             "y": 96
@@ -13,7 +13,7 @@ var json =
     {
         "title": "participation_1",
         "tags": "",
-        "body": "Starting with participation. It reflects how much time you spoke during the whole conversation.\n[[Got it|participation_2]]",
+        "body": "Let's start with participation, which reflects how much you spoke during the whole conversation.\n[[Okay|participation_2]]",
         "position": {
             "x": 301,
             "y": 96
@@ -33,7 +33,7 @@ var json =
     {
         "title": "participation_4",
         "tags": "",
-        "body": "Team management experts have found that, equal participation is healthy for a group.\\nSpeaking about your own opinion can remove confusions, whereas listening to teammates' viewpoints is also valuable.\\nRemember, balance is the key!\n[[Seems useful|overlap_1]]",
+        "body": "Team management experts have found that, equal participation is healthy for a group.\\nThis is because speaking your own opinion and listening to others can prevent confusions.\\nRemember, balance is the key!\n[[Got it!|overlap_1]]",
         "position": {
             "x": 839,
             "y": 99
@@ -53,7 +53,7 @@ var json =
     {
         "title": "overlap_2",
         "tags": "",
-        "body": "Speech overlap happens when someone speaks before another person finishes.\\nSometimes these can encourage the conversation, but sometimes can be unpleasant as well.\\nJust something to keep in mind.\\n [[Good to know|overlap_3]]",
+        "body": "Speech overlap happens when someone speaks before another person finishes.\\nOverlapping can show enthusiasm and drive conversations forward. But sometimes they can be seen as unwanted interruptions.\\nJust something to keep in mind.\\n [[Good to know|overlap_3]]",
         "position": {
             "x": 179,
             "y": 141
@@ -73,7 +73,7 @@ var json =
     {
         "title": "turntaking_1",
         "tags": "",
-        "body": "Speaking of overlap, it also plays a key role in deciding who speaks next. Let's find out who you usually talked after.\n [[Interesting|turntaking_2]]",
+        "body": "Speaking of overlaps, they also plays a key role in deciding who speaks next. Let's find out who you usually talked after.\n [[Yeah, let's do this!|turntaking_2]]",
         "position": {
             "x": -36,
             "y": 395
@@ -93,7 +93,7 @@ var json =
     {
         "title": "emotion_1",
         "tags": "",
-        "body": "Time for discussing valence.\\nValence, in other words - emotion range, can both derive from and affect group discussion.\n[[Show mine|emotion_2]]",
+        "body": "Let's discuss valence.\\nValence, in other words - emotion range, can both derive from and affect group discussion.\n[[Show mine|emotion_2]]",
         "position": {
             "x": 28,
             "y": 418
@@ -113,7 +113,7 @@ var json =
     {
         "title": "emotion_3",
         "tags": "1",
-        "body": "\n[[Why is this immportant?|emotion_4]] [[I got this|sharedsmile_0]]",
+        "body": "\n[[Why is this important?|emotion_4]] [[I got this|sharedsmile_0]]",
         "position": {
             "x": 576,
             "y": 415
@@ -123,7 +123,7 @@ var json =
     {
         "title": "emotion_4",
         "tags": "",
-        "body": "Positive sentiments can improve cooperation within the group, whereas negative ones can hurt it.\\nThese can even affect the bonds within the group members.\n[[Sounds powerful|sharedsmile_0]]",
+        "body": "Positive sentiments can improve cooperation within the group, whereas negative ones can hurt it.\\nThese can even affect the bonds within the group members.\n[[Interesting|sharedsmile_0]]",
         "position": {
             "x": 860,
             "y": 413
@@ -143,7 +143,7 @@ var json =
     {
         "title": "sharedsmile_1",
         "tags": "",
-        "body": "Great! So first let me tell you what it represents.\\nShared smile is how much time two persons smile simultaneously. It can express a lot about group dynamics.\n[[Show my score|sharedsmile_2]]",
+        "body": "Great! Let me tell you what the graph represents.\\nShared smiles are when two people smile at the same time. It can express a lot about group dynamics.\n[[Show my score|sharedsmile_2]]",
         "position": {
             "x": 32,
             "y": 702
@@ -163,7 +163,7 @@ var json =
     {
         "title": "finish_1",
         "tags": "",
-        "body": "This is all I have. Good job expressing!\\nAll these feedback might help you in your future team meetings.\\nHope you have a great group discussion next time!\n[[Thanks, bye!|]]",
+        "body": "That's all for today.\\nI hope this feedback helps you in your future team meetings. :)\n[[Thanks, bye!|]]",
         "position": {
             "x": 589,
             "y": 704
