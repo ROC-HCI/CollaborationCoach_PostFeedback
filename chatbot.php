b8d0a9092e8e78bbb011b5e59fc35a4e48140a4e
ssmumu <samiha.mumu@gmail.com>
2016-11-06 15:51:39 -0500

    stop thinking

--- chatbot.php
+++ chatbot.php
@@ -95,6 +95,7 @@
 		console.log(participation);
 		var thinking = $('#messages li:last');
 		var thinkingflag = 0;
+		var countdelay = 0;
 		$(document).ready(function(){
 			gotoObject(participation[Object.keys(participation)[0]]); //start from the initial item on the list
 			gatherData();
@@ -139,6 +140,7 @@
     				},2000);
     				setTimeout(function(){
     					createGraph(graphType[countType]);
+    					thinking.remove();   
 	    				$('#messages').append(new item("Roboto", graphResponse[countType]).create());	
     					countType++;
     				},2400);
@@ -146,14 +148,14 @@
         	}
         }
         function fixNewline(obj){
-        	var count = 1;
+        	countdelay = 1;
         
         	for(var str of participation[obj.title].body.split(/\\n/)){
         		console.log('whats my string ',str.length);
         		if(!/\S/.test(str)) continue;
         		(function(str){
         			setTimeout(function(){
-						thinking.remove();        			
+					      			
         				$('#messages').append(new item("Roboto", str).create());	
         				$('.inner-contain-body').animate({ 
         				scrollTop: $('#messages').height()
