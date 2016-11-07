bfb684aadf9afc280c66d4941366f7545cd77b03
ssmumu <samiha.mumu@gmail.com>
2016-11-06 21:26:48 -0500

    oka

--- chatbot.php
+++ chatbot.php
@@ -163,7 +163,7 @@
 						});
 
     				},(countdelay)*1000);
-    				countdelay+=3.0;	
+    				countdelay+=3.3;	
 
         			console.log('when empty1 ',countdelay);
         			console.log('when thinkingflag ',thinkingflag);
@@ -358,7 +358,7 @@
     		$('.inner-contain-graph').animate({ 
 			//      scrollTop: $('.wrapper:last').height()+$('#accordion').height()
 			}, function() {
-				$('#wrapper-' + type).show("slide", { direction: "down" }, 2200);
+				$('#wrapper-' + type).show("slide", { direction: "down" }, 2300);
 			});
 			make_Graph(type);
 			$('.inner-contain-graph').animate({ 
