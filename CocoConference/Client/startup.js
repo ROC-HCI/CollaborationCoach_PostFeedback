90a6bca6c4a70bb38d1c6234aac37cbdf3c2059d
jwhite37 <jwhite37@ur.rochester.edu>
2016-12-04 15:20:23 -0500

    rollback

--- CocoConference/Client/startup.js
+++ CocoConference/Client/startup.js
@@ -274,7 +274,7 @@ function setup_local_media(callback, errorback)
 			local_media.attr("autoplay", "autoplay");
 			local_media.attr("muted", "true"); /* always mute ourselves by default */
 			local_media.attr("controls", "");
-			$('body').append(local_media);			
+			$('body').append(local_media);
 			attachMediaStream(local_media[0], stream);
 
 			if (callback) callback();
