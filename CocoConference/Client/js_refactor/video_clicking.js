91edd030ff8cf1a4e1e249525cb676775fbbd03d
jwhite37 <jwhite37@ur.rochester.edu>
2017-02-19 14:42:48 -0500

    click update

--- CocoConference/Client/js_refactor/video_clicking.js
+++ CocoConference/Client/js_refactor/video_clicking.js
@@ -3,10 +3,8 @@ var bigVid = document.getElementById("focus_feed");
 
 $("#remote_videos").on("hover", function(event)
 {
-	console.log("HOVER FUNCTION CALLED");
 	if (event.target != this)
 	{
-		console.log("HOVER DETECTED");
 		bigVid.innerHTML = "";
 		$(event.target).clone().appendTo(bigVid);
 	}
