381293547958804a030115af1539bc45b27e3a4e
jwhite37 <jwhite37@ur.rochester.edu>
2017-02-22 12:47:56 -0500

    canvas retest

--- CocoConference/Client/js_refactor/video_clicking.js
+++ CocoConference/Client/js_refactor/video_clicking.js
@@ -4,10 +4,10 @@ $("#remote_videos").on("hover", function(event)
 {
 	if (event.target != this)
 	{
-		$("#focus_video").src = event.target.src;
+		//$("#focus_video").src = event.target.src;
 		$("#focus_target").val(event.target.id);
-		$("#focus_video").play();
-		/*
+		//$("#focus_video").play();
+		
 		switch_video = true;
 		
 		var video = event.target;
@@ -16,7 +16,6 @@ $("#remote_videos").on("hover", function(event)
 		
 		switch_video = false;
 		draw(video, context, 200, 200);
-		*/
 		
 		//bigVid.innerHTML = "";
 		//$(event.target).clone().appendTo(bigVid);
@@ -26,7 +25,11 @@ $("#remote_videos").on("hover", function(event)
 
 function draw(video, context, width, height)
 {
-	if(video.paused || video.ended || switch_video) return false;
+	if(video.paused || video.ended || switch_video) 
+	{
+		console.log("killed draw function on switch");
+		return false;
+	}
 	
 	context.drawImage(video, 0, 0, width, height);
 	setTimeout(draw, 20, video, context, width, height);
