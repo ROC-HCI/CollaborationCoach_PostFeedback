7d527d73266c00767ca6169117d4b0242ee36f7b
jwhite37 <jwhite37@ur.rochester.edu>
2017-03-19 16:08:15 -0400

    fix

--- CocoConference/Client/js_experiment/session-two.js
+++ CocoConference/Client/js_experiment/session-two.js
@@ -2,7 +2,7 @@
 
 var experiment_instructions = "SCENARIO: You are a member of a space crew originally scheduled to rendezvous with a mother ship on the lighted surface of the moon. However, due to mechanical difficulties, your ship was forced to land at a spot some 200 miles from the rendezvous point. During reentry and landing, much of the equipment aboard was damaged and, since survival depends on reaching the mother ship, the most critical items available must be chosen for the 200-mile trip. On your own sort the items on the next screen based on how you feel they should be ranked (top being highest priority) and press 'submit'. Then the video call will begin, where you and your group will decide on the top three items.";
 
-var current_rankings = nulll;
+var current_ranking = nulll;
 
 function submit_answers(selections)
 {
@@ -18,7 +18,7 @@ function submit_answers(selections)
 		}
 	};
 
-	current_rankings = selections;
+	current_ranking = selections;
 	
 	data_to_send = {'session_key':session_key,
 					'user':user_name,
