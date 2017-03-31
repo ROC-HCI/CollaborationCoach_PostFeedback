bdb6638e42978f01d187b8ff99cb0dcc5a433659
jwhite37 <jwhite37@ur.rochester.edu>
2017-03-31 18:12:01 -0400

    test

--- runscript.sh
+++ runscript.sh
@@ -4,14 +4,20 @@ session_id=$1
 
 echo "Session Script Starting for: $session_id"
 
-echo "Start - .webm Header Correction"
-for file in Data/$session_id*.webm;
-	do ffmpeg -i "$file" -c copy -fflags +genpts Data/fixed_$(basename "${file/.webm}").webm
-done
-echo "Finish - .webm Header Correction"
+#echo "Start - .webm Header Correction"
+#for file in Data/$session_id*.webm;
+#	do ffmpeg -i "$file" -c copy -fflags +genpts Data/fixed_$(basename "${file/.webm}").webm
+#done
+#echo "Finish - .webm Header Correction"
+
+#echo "Start - .wav Conversion"
+#for file in Data/fixed_$session_id*.webm;
+#	do ffmpeg -i "$file" Data/$(basename "${file/.webm}").wav
+#done
+#echo "Finish - .wav Conversion"
 
 echo "Start - .wav Conversion"
-for file in Data/fixed_$session_id*.webm;
+for file in Data/$session_id*.webm;
 	do ffmpeg -i "$file" Data/$(basename "${file/.webm}").wav
 done
 echo "Finish - .wav Conversion"
@@ -22,15 +28,27 @@ echo "Finish - .wav Conversion"
 #done
 #echo "End - .flacc conversion"
 
+#echo "Start - Praat"
+#for file in Data/fixed_$session_id*.wav;
+#	do ./praat --run auto.praat $file
+#done
+#echo "Finish - Praat"
+
+#echo "Start - Participation Analysis"
+#argpath=""
+#for i in Data/fixed_$session_id*.wav.TextGrid;
+#	do argpath="$argpath $i"
+#done
+
 echo "Start - Praat"
-for file in Data/fixed_$session_id*.wav;
+for file in Data/$session_id*.wav;
 	do ./praat --run auto.praat $file
 done
 echo "Finish - Praat"
 
 echo "Start - Participation Analysis"
 argpath=""
-for i in Data/fixed_$session_id*.wav.TextGrid;
+for i in Data/$session_id*.wav.TextGrid;
 	do argpath="$argpath $i"
 done
 
