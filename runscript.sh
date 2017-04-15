571fd9732a90a199c594c653977c7f2376f74bdf
jwhite37 <jwhite37@ur.rochester.edu>
2017-04-15 16:19:59 -0400

    vp8 testing

--- runscript.sh
+++ runscript.sh
@@ -6,25 +6,25 @@ echo "Session Script Starting for: $session_id"
 
 echo "Start - .webm Header Correction"
 for file in Data/$session_id*.webm;
-	do ffmpeg -i "$file" -codec:v libvpx -quality good -cpu-used 0 -b:v 500k -qmin 10 -qmax 42 -maxrate 500k -bufsize 1000k -threads 4 -vf scale=-1:480 -codec:a libvorbis -b:a 128k Data/testFixed1_$(basename "${file/.webm}").webm
+	do ffmpeg -i "$file" -codec:v libvpx -quality good -cpu-used 0 -b:v 500k -qmin 10 -qmax 42 -maxrate 500k -bufsize 1000k -threads 4 -vf scale=-1:480 -codec:a libvorbis -b:a 128k Data/testFixed2_$(basename "${file/.webm}").webm
 done
 echo "Finish - .webm Header Correction"
 
 echo "Start - .wav Conversion"
-for file in Data/testFixed1_$session_id*.webm;
+for file in Data/testFixed2_$session_id*.webm;
 	do ffmpeg -i "$file" -ar 11025 Data/$(basename "${file/.webm}").wav
 done
 echo "Finish - .wav Conversion"
 
 echo "Start - Praat"
-for file in Data/testFixed1_$session_id*.wav;
+for file in Data/testFixed2_$session_id*.wav;
 	do ./praat --run auto.praat $file
 done
 echo "Finish - Praat"
 
 echo "Start - Participation Analysis"
 argpath=""
-for i in Data/testFixed1_$session_id*.wav.TextGrid;
+for i in Data/testFixed2_$session_id*.wav.TextGrid;
 	do argpath="$argpath $i"
 done
 
