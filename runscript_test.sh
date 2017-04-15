be2b180a48bb64f9a14cea2fb572a6403cb1ce61
jwhite37 <jwhite37@ur.rochester.edu>
2017-04-15 15:30:55 -0400

    testing

--- runscript_test.sh
+++ runscript_test.sh
@@ -4,27 +4,27 @@ session_id=$1
 
 echo "Session Script Starting for: $session_id"
 
-#echo "Start - .webm Header Correction"
-#for file in Data/$session_id*.webm;
-#	do ffmpeg -i "$file" -v:c copy -a:c copy Data/fixedTST8_$(basename "${file/.webm}").webm
-#done
-#echo "Finish - .webm Header Correction"
+echo "Start - .webm Header Correction"
+for file in Data/$session_id*.webm;
+	do ffmpeg -i "$file" -v:c copy -a:c copy Data/testFixed1_$(basename "${file/.webm}").webm
+done
+echo "Finish - .webm Header Correction"
 
 echo "Start - .wav Conversion"
-for file in Data/fixedTST7_$session_id*.webm;
+for file in Data/testFixed1_$session_id*.webm;
 	do ffmpeg -i "$file" Data/$(basename "${file/.webm}").wav
 done
 echo "Finish - .wav Conversion"
 
 echo "Start - Praat"
-for file in Data/fixedTST7_$session_id*.wav;
+for file in Data/testFixed1_$session_id*.wav;
 	do ./praat --run auto.praat $file
 done
 echo "Finish - Praat"
 
 echo "Start - Participation Analysis"
 argpath=""
-for i in Data/fixedTST7_$session_id*.wav.TextGrid;
+for i in Data/testFixed1_$session_id*.wav.TextGrid;
 	do argpath="$argpath $i"
 done
 
