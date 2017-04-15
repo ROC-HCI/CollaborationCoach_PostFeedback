0d891d2b814d2c40c3223c1e7baf6c46303e4fd3
jwhite37 <jwhite37@ur.rochester.edu>
2017-04-15 12:52:23 -0400

    testing

--- runscript_test.sh
+++ runscript_test.sh
@@ -4,32 +4,32 @@ session_id=$1
 
 echo "Session Script Starting for: $session_id"
 
-#echo "Start - .webm Header Correction"
-#for file in Data/$session_id*.webm;
-#	do ffmpeg -i "$file" -c copy -fflags +genpts Data/fixed_$(basename "${file/.webm}").webm
-#done
-#echo "Finish - .webm Header Correction"
+echo "Start - .webm Header Correction"
+for file in Data/$session_id*.webm;
+	do ffmpeg -i "$file" -c copy -fflags +genpts -codec:v libvpx Data/fixedTST6_$(basename "${file/.webm}").webm
+done
+echo "Finish - .webm Header Correction"
 
 echo "Start - .wav Conversion"
-for file in Data/$session_id*.webm;
-	do ffmpeg -i "$file" -acodec pcm_u8 -ar 11025 Data/fixedTST5_$(basename "${file/.webm}").wav
+for file in Data/fixedTST6_$session_id*.webm;
+	do ffmpeg -i "$file" Data/fixedTST6_$(basename "${file/.webm}").wav
 done
 echo "Finish - .wav Conversion"
 
 echo "Start - Praat"
-for file in Data/fixedTST5_$session_id*.wav;
+for file in Data/fixedTST6_$session_id*.wav;
 	do ./praat --run auto.praat $file
 done
 echo "Finish - Praat"
 
 echo "Start - Participation Analysis"
 argpath=""
-for i in Data/fixedTST5_$session_id*.wav.TextGrid;
+for i in Data/fixedTST6_$session_id*.wav.TextGrid;
 	do argpath="$argpath $i"
 done
 
-python fileparser.py $argpath
-echo "Finish - Participation Analysis"
+#python fileparser.py $argpath
+#echo "Finish - Participation Analysis"
 
 #echo "Start - average affdex features"
 #python AffdexParser.py $session_id
