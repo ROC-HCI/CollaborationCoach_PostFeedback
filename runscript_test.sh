694182bfa91732e09323c462264ea3d2c16a07eb
jwhite37 <jwhite37@ur.rochester.edu>
2017-04-15 11:53:04 -0400

    test

--- runscript_test.sh
+++ runscript_test.sh
@@ -4,20 +4,20 @@ session_id=$1
 
 echo "Session Script Starting for: $session_id"
 
-echo "Start - .webm Header Correction"
-for file in Data/$session_id*.webm;
-	do ffmpeg -i "$file" -c copy -fflags +genpts Data/fixed_$(basename "${file/.webm}").webm
-done
-echo "Finish - .webm Header Correction"
+#echo "Start - .webm Header Correction"
+#for file in Data/$session_id*.webm;
+#	do ffmpeg -i "$file" -c copy -fflags +genpts Data/fixed_$(basename "${file/.webm}").webm
+#done
+#echo "Finish - .webm Header Correction"
 
 echo "Start - .wav Conversion"
-for file in Data/fixed_$session_id*.webm;
-	do ffmpeg -i "$file" Data/fixedTST_$(basename "${file/.webm}").wav
+for file in Data/$session_id*.webm;
+	do ffmpeg -i "$file" Data/fixedTST2_$(basename "${file/.webm}").wav
 done
 echo "Finish - .wav Conversion"
 
 echo "Start - Praat"
-for file in Data/fixedTST_$session_id*.wav;
+for file in Data/fixedTST2_$session_id*.wav;
 	do ./praat --run auto.praat $file
 done
 echo "Finish - Praat"
