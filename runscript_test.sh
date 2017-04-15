5447ce829b4d3df3906ad8e198f205023f65daf9
jwhite37 <jwhite37@ur.rochester.edu>
2017-04-15 11:24:27 -0400

    update

---
+++ runscript_test.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+session_id=$1
+
+echo "Session Script Starting for: $session_id"
+
+#echo "Start - .webm Header Correction"
+#for file in Data/$session_id*.webm;
+#	do ffmpeg -i "$file" -c copy -fflags +genpts Data/fixed_$(basename "${file/.webm}").webm
+#done
+#echo "Finish - .webm Header Correction"
+
+echo "Start - .wav Conversion"
+for file in Data/$session_id*.webm;
+	do ffmpeg -i "$file" Data/fixed_$(basename "${file/.webm}").wav
+done
+echo "Finish - .wav Conversion"
+
+echo "Start - Praat"
+for file in Data/fixed_$session_id*.wav;
+	do ./praat --run auto.praat $file
+done
+echo "Finish - Praat"
+
+echo "Start - Participation Analysis"
+argpath=""
+for i in Data/fixed_$session_id*.wav.TextGrid;
+	do argpath="$argpath $i"
+done
+
+python fileparser.py $argpath
+echo "Finish - Participation Analysis"
+
+#echo "Start - average affdex features"
+#python AffdexParser.py $session_id
+#echo "Finish - average affdex features"
+
+#echo "Start - shared affdex features"
+#python SharedAffdexDetection.py $session_id
+#echo "Finish - shared affdex features"
+
+echo "Session Script Terminated..."
+
