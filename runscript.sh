55bbf740e11425c87c6a35f72d11a1c89f99f61f
hitejwhite37 <jwhite37@ur.rochester.edu>
2017-04-27 14:51:37 -0400

    testing pratt

--- runscript.sh
+++ runscript.sh
@@ -4,28 +4,28 @@ session_id=$1
 
 echo "Session Script Starting for: $session_id"
 
-echo "Start - Header Correction"
-for file in Data/$session_id*.webm;
-	do ffmpeg -i "$file" -codec:v copy -codec:a aac Data/$(basename "${file/.webm}").mp4
-done
-echo "Finish - Header Correction"
+#echo "Start - Header Correction"
+#for file in Data/$session_id*.webm;
+#	do ffmpeg -i "$file" -codec:v copy -codec:a aac Data/$(basename "${file/.webm}").mp4
+#done
+#echo "Finish - Header Correction"
 
-echo "Start - .wav Conversion"
-for file in Data/$session_id*.mp4;
-	do if [[ $file != *"LENOVO"* ]]
-	then
-		echo "it's h264"
-		ffmpeg -i "$file" -ar 11025 Data/fixed_$(basename "${file/.mp4}").wav
-	fi
-done
-for file in Data/$session_id*.webm;
-	do if [[ $file == *"LENOVO"* ]]
-	then
-    	echo "it's vp8"
-    	ffmpeg -i "$file" -ar 11025 Data/fixed_$(basename "${file/.webm}").wav
-	fi
-done
-echo "Finish - .wav Conversion"
+#echo "Start - .wav Conversion"
+#for file in Data/$session_id*.mp4;
+#	do if [[ $file != *"LENOVO"* ]]
+#	then
+#		echo "it's h264"
+#		ffmpeg -i "$file" -ar 11025 Data/fixed_$(basename "${file/.mp4}").wav
+#	fi
+#done
+#for file in Data/$session_id*.webm;
+#	do if [[ $file == *"LENOVO"* ]]
+#	then
+#   	echo "it's vp8"
+#   	ffmpeg -i "$file" -ar 11025 Data/fixed_$(basename "${file/.webm}").wav
+#	fi
+#done
+#echo "Finish - .wav Conversion"
 
 echo "Start - Praat"
 for file in Data/fixed_$session_id*.wav;
@@ -33,21 +33,21 @@ for file in Data/fixed_$session_id*.wav;
 done
 echo "Finish - Praat"
 
-echo "Start - Participation Analysis"
-argpath=""
-for i in Data/fixed_$session_id*.wav.TextGrid;
-	do argpath="$argpath $i"
-done
+#echo "Start - Participation Analysis"
+#argpath=""
+#for i in Data/fixed_$session_id*.wav.TextGrid;
+#	do argpath="$argpath $i"
+#done
 
-python fileparser.py $argpath
-echo "Finish - Participation Analysis"
+#python fileparser.py $argpath
+#echo "Finish - Participation Analysis"
 
-echo "Start - average affdex features"
-python AffdexParser.py $session_id
-echo "Finish - average affdex features"
+#echo "Start - average affdex features"
+#python AffdexParser.py $session_id
+#echo "Finish - average affdex features"
 
-echo "Start - shared affdex features"
-python SharedAffdexDetection.py $session_id
-echo "Finish - shared affdex features"
+#echo "Start - shared affdex features"
+#python SharedAffdexDetection.py $session_id
+#echo "Finish - shared affdex features"
 
 echo "Session Script Terminated..."
