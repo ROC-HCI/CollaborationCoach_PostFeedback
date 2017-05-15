f9b3571bb0c97e2446d71069f03ad7f7ae6c3ac0
hitejwhite37 <jwhite37@ur.rochester.edu>
2017-05-15 13:10:17 -0400

    set for file regeneration

--- runscript.sh
+++ runscript.sh
@@ -27,27 +27,27 @@ for file in Data/$session_id*.webm;
 done
 echo "Finish - .wav Conversion"
 
-echo "Start - Praat"
-for file in Data/fixed_$session_id*.wav;
-	do ./praat --run auto.praat $file
-done
-echo "Finish - Praat"
+#echo "Start - Praat"
+#for file in Data/fixed_$session_id*.wav;
+#	do ./praat --run auto.praat $file
+#done
+#echo "Finish - Praat"
 
-echo "Start - Participation Analysis"
-argpath=""
-for i in Data/fixed_$session_id*.wav.TextGrid;
-	do argpath="$argpath $i"
-done
+#echo "Start - Participation Analysis"
+#argpath=""
+#for i in Data/fixed_$session_id*.wav.TextGrid;
+#	do argpath="$argpath $i"
+#done
 
-python fileparser.py $argpath
-echo "Finish - Participation Analysis"
+#python fileparser.py $argpath
+#echo "Finish - Participation Analysis"
 
-echo "Start - average affdex features"
-python AffdexParser.py $session_id
-echo "Finish - average affdex features"
+#echo "Start - average affdex features"
+#python AffdexParser.py $session_id
+#echo "Finish - average affdex features"
 
-echo "Start - shared affdex features"
-python SharedAffdexDetection.py $session_id
-echo "Finish - shared affdex features"
+#echo "Start - shared affdex features"
+#python SharedAffdexDetection.py $session_id
+#echo "Finish - shared affdex features"
 
 echo "Session Script Terminated..."
